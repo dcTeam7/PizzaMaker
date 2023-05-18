81e38a25ec677fd848a2f1b1425d728e5119f939
AbraaoAlves <abraao.teodosio@gmail.com>
2023-05-18 10:21:43 -0300

    generate-image: commented files challenge

---
+++ server/generate-image.js
@@ -0,0 +1,14 @@
+// import mergeImages from 'merge-images';
+// https://www.npmjs.com/package/merge-images
+
+export default async () => {
+
+    // usar o mergeImages para mesclar as tres imagens
+    // imgs/borda-01.png, imgs/molho-vermelho.png imgs/recheio-01.png
+    
+    // receber a base64
+    // transformar a base64 em arquivo 
+    // salvar o arquivo no diretorio server/output.png
+
+    return // path para o output.png
+}
